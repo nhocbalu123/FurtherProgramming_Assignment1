e0c27e637315c358c7f2d91b68738c6eeb21db7b
nhocbalu123 <85002041+nhocbalu123@users.noreply.github.com>
2024-04-01 19:58:10 +0700

    add all methods to ClaimProcessManagerImp

--- src/Assignment1/ClaimProcessManagerImp.java
+++ src/Assignment1/ClaimProcessManagerImp.java
@@ -20,8 +20,27 @@ public class ClaimProcessManagerImp implements ClaimProcessManager {
         for (int i = 0; i < claims.size(); i++) {
             if (claims.get(i).getId().equals(claim.getId())) { //check if there is the correct claim ID
                 claims.set(i, claim);
-                break;
+                break; // break out once found the ID
             }
         }
+    }
+
+    //Delete claim
+    public void delete(Claim claim) {
+        claims.removeIf(c -> c.getId().equals(claim.getId()));
+    }
+
+    //Return one claim
+    public Claim getOne(String id) {
+        for (Claim claim : claims) {
+            if (claim.getId().equals(id)) {
+                return claim;
+            }
+        }
+        return null;
+    }
+    //Return list of claims
+    public List<Claim> getAll() {
+        return claims;
     }
 }

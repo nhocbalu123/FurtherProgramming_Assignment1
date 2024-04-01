011e8916f42f1875cdd627d6ae663438d8b0253a
nhocbalu123 <85002041+nhocbalu123@users.noreply.github.com>
2024-04-01 18:21:33 +0700

    create Customer class

---
+++ src/Assignment1/Customer.java
@@ -0,0 +1,10 @@
+package Assignment1;
+/**
+ * @author <Nguyen Quy Minh Thang - s3978302>
+ */
+public class Customer {
+    private String id;
+    private String fullName;
+    private InsuranceCard card;
+    private List<Claim> claims;
+}
